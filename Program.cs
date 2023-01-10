8570db36361686ead4aa8b728909ce50a3ef365e
vas <vas.grishko@yandex.ru>
2023-01-10 23:06:12 +0300

    Написали программу

--- Program.cs
+++ Program.cs
@@ -1,40 +1,6 @@
-﻿string[] arr =  {"Hello","am",
-                     "is",
-                     "mah!",
-                     "Kitteh","I'm 1337",
-                     "Your the machine, I'm the human.",
-                     "Fire your own laser.",
-                     "Shut up noob.",
-                     "Luk, I am your father.",
-                     "Kitteh","Orly?",
-                     "I was made in C#.",
-                     "Randomness!",
-                     "Are you sure?",
-                     "Yeah, me too.",
-                     "What are you talking about?",
-                     "Kids these days","I know right?",
-                     "Icecream is good.",
-                     "#include i<ostream>",
-                     "Man, I'm bored.",
-                     "Just shut up will ya?",
-                     "Are you sure?","Yeah!",
-                     "As",
-                     "You made me angry",
-                     "Uh",
-                     "What is wrong with you?",
-                     "Oog","733","Blobbeh",
-                     "Turtlegiant",
-                     "If","And","Or",
-                     "day",
-                     "I'm truly random.",
-                     "Are you?",
-                     "I'm a pretty smart AI.",
-                     "Yarly.","Kitteh Behind Yeh!.",
-                     "Man I'm bored","Your not making any sense",
-                     "Never!!!","lol",
-                     "Rofl","I'm hungry","My food!"};
+﻿string[] arr = {"Russia","KFC","Denmark","Man","Georgia","I","The", "Me","End","USA","Kazan","Too",
+"Brazil","Wow","Ape","Poland","UFC","England","OK"};
 
-// Создаем метод вывода в консоль заданого массива
 void PrintArray(string[] arr)
 {
     Console.Write("[");
@@ -45,7 +11,7 @@ void PrintArray(string[] arr)
     }
     Console.WriteLine("]");
 }
-// Создаем метод вывода в консольнового массива
+
 void PrintArraySorting(string[] sortingArray)
 {
     for (int i = 0; i < sortingArray.Length; i++)
@@ -53,7 +19,7 @@ void PrintArraySorting(string[] sortingArray)
         Console.Write(sortingArray[i] + " ");
     }
 }
-// Создаем метод отсеивания из массива элементов, которые больше 3-х знаков
+
 string[] SortingArray(string[] array)
 {
     string[] sortingArray = new string[array.Length];
@@ -71,6 +37,6 @@ string[] SortingArray(string[] array)
 
 Console.WriteLine("Выводим первоначальный массив: ");
 PrintArray(arr);
-Console.WriteLine("Выводим отсортированный массив с длинной не более 3 символов в элементе: ");
+Console.WriteLine("Выводим отсортированный массив с длиной не более 3 символов в элементе: ");
 string[] sortingArray = SortingArray(arr);
 PrintArraySorting(sortingArray);
